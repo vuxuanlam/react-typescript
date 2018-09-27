a0cc7ae568b89a4cf19af8931ef365f1fe3d45c4
vuxuanlam <lam.vuxuan2512@gmail.com>
2018-09-28 01:20:09 +0900

    add reset and setting size component

--- src/components/ColorApp.tsx
+++ src/components/ColorApp.tsx
@@ -9,7 +9,7 @@ export default class ColorApp extends React.Component<any, any>{
         super(props)
         this.state = {
             color: 'red',
-            fontsize: 15
+            fontSize: 15
         }
     }
 
@@ -17,6 +17,23 @@ export default class ColorApp extends React.Component<any, any>{
         this.setState({
             color: params
         });
+    }
+    onChangeSize = (value: any) => {
+        // this.onChangeSize(params);
+        // console.log(value)
+        this.setState({
+            fontSize: ((this.state.fontSize + value <= 36) && (this.state.fontSize + value >= 8)) ? this.state.fontSize + value : this.state.fontSize
+        })
+
+    };
+
+    onReset = (value: boolean) => {
+        if (value) {
+            this.setState({
+                color: 'red',
+                fontSize: 15
+            });
+        }
     }
 
     render() {
@@ -27,12 +44,12 @@ export default class ColorApp extends React.Component<any, any>{
                         <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor} />
                     </div>
                     <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                        <SizeSetting />
-                        <Reset />
+                        <SizeSetting fontSize={this.state.fontSize} onChangeSize={this.onChangeSize} />
+                        <Reset onReset={this.onReset} />
                     </div>
                 </div>
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-50">
-                    <Result color={this.state.color} />
+                    <Result color={this.state.color} fontSize={this.state.fontSize} />
                 </div>
             </div>
         )
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -5,16 +5,17 @@ class Result extends React.Component<any, any> {
     setStyle() {
         return {
             color: this.props.color,
-            borderColor: this.props.color
+            borderColor: this.props.color,
+            fontSize: this.props.fontSize
         };
     }
     public render() {
         return (
             <div className="panel panel-info">
                 <div className="panel-heading">
-                    <h3 className="panel-title">Panel title</h3>
+                    <h3 className="panel-title">Color : {this.props.color} - Size : {this.props.fontSize} </h3>
                 </div>
-                <div id="content" className="panel-body" style={this.setStyle()}>
+                <div id="content" style={this.setStyle()}>
                     Panel content
                 </div>
             </div>
